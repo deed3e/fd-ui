772c1e4fa0e145dcbda8af2e2b5e70e59e03a3fb
deed3e <hungvt164@gmail.com>
2023-07-23 21:49:14 +0700

    format

--- src/view/Share/Footer/index.tsx
+++ src/view/Share/Footer/index.tsx
@@ -1,82 +1,72 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import logo from '../../../assets/icon/logo-tab-3.png';
-import tw from "../../../assets/icon/ic-tw.svg";
-import tg from "../../../assets/icon/ic-tg.svg";
-import { screenUp } from "../../../utils/styles";
+import tw from '../../../assets/icon/ic-tw.svg';
+import tg from '../../../assets/icon/ic-tg.svg';
+import { screenUp } from '../../../utils/styles';
 const Footer: React.FC = () => {
-    return (
-        <>
-            <StyledContainer>
-                <StyledTop>
-                    <div className="left">
-                        <div className="logo">
-                            <img src={logo} />
-                        </div>
-                    </div>
-                    <div className="right">
-                        <StyledLinks>
-                            <div className="icon">
-                                <img src={tw} />
-                            </div>
-                            <div className="icon">
-                                <img src={tg} />
-                            </div>
-                        </StyledLinks>
-                    </div>
-                </StyledTop>
-                <StyledBody>
-                      <div>
-                      owner@miladyraces.bnb
-                      </div>
-                      <div>
-                      Copyright Milady
-                      </div>
-                </StyledBody>
-
-            </StyledContainer>
-        </>
-    );
-}
+  return (
+    <>
+      <StyledContainer>
+        <StyledTop>
+          <div className="left">
+            <div className="logo">
+              <img src={logo} />
+            </div>
+          </div>
+          <div className="right">
+            <StyledLinks>
+              <div className="icon">
+                <img src={tw} />
+              </div>
+              <div className="icon">
+                <img src={tg} />
+              </div>
+            </StyledLinks>
+          </div>
+        </StyledTop>
+        <StyledBody>
+          <div>owner@miladyraces.bnb</div>
+          <div>Copyright Milady</div>
+        </StyledBody>
+      </StyledContainer>
+    </>
+  );
+};
 
 export default Footer;
 
 const StyledContainer = styled.div`
-     padding: 10px;
-     background-color: black;
-     display: flex;
-     flex-direction: column;
-     color: #fff;
-  `;
+  padding: 10px;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+`;
 
 const StyledTop = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    .left img{
-          height: 100px;
-          padding-left: 50px;
-    }
-    ${screenUp('lg')`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  .left img {
+    height: 100px;
+    padding-left: 50px;
+  }
+  ${screenUp('lg')`
          justify-content: space-between;
          flex-direction:row;
     `}
-    flex-direction:column;
+  flex-direction:column;
 `;
 
 const StyledLinks = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-right: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 50px;
 `;
 const StyledBody = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
 `;
-
-
-
-
-
