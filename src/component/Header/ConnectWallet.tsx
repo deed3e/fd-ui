f453be1d1ca2be29d24d652ba9dc5f799935d28d
deed3e <hungvt164@gmail.com>
2023-09-04 00:18:35 +0700

    deploy

--- src/component/Header/ConnectWallet.tsx
+++ src/component/Header/ConnectWallet.tsx
@@ -7,67 +7,67 @@ import { useEffect } from 'react';
 import { useConnect, useAccount, useDisconnect, useSwitchNetwork } from 'wagmi';
 
 const ConnectWallet: React.FC = () => {
-  const { connect, connectors, isSuccess } = useConnect();
-  const { disconnect } = useDisconnect();
-  const { address, isConnected } = useAccount();
-  const { switchNetwork } = useSwitchNetwork();
+    const { connect, connectors, isSuccess } = useConnect();
+    const { disconnect } = useDisconnect();
+    const { address, isConnected } = useAccount();
+    const { switchNetwork } = useSwitchNetwork();
 
-  const connector = connectors.findLast((connector) => connector.ready);
+    const connector = connectors.findLast((connector) => connector.ready);
 
-  const handleConnectWallet = async () => {
-    if (!connector) {
-      alert('Please install MetaMask!');
-      return;
-    }
-    connect({ connector });
-  };
+    const handleConnectWallet = async () => {
+        if (!connector) {
+            alert('Please install MetaMask!');
+            return;
+        }
+        connect({ connector });
+    };
 
-  useEffect(() => {
-    if (isSuccess) {
-      switchNetwork?.(97);
-    }
-  }, [isSuccess, switchNetwork]);
+    useEffect(() => {
+        if (isSuccess) {
+            switchNetwork?.(97);
+        }
+    }, [isSuccess, switchNetwork]);
 
-  return (
-    <>
-      {!isConnected && (
-        <StyledButtonConnect onClick={handleConnectWallet}>
-          <IconConnectWallet />
-          <span>CONNECT</span>
-        </StyledButtonConnect>
-      )}
-      {isConnected && (
-        <StyledButtonConnected onClick={()=>disconnect()}>
-          <StyledLogo src={userIcon} />
-          <span className="address">{shortenAddress(address)}</span>
-        </StyledButtonConnected>
-      )}
-    </>
-  );
+    return (
+        <>
+            {!isConnected && (
+                <StyledButtonConnect onClick={handleConnectWallet}>
+                    <IconConnectWallet />
+                    <span>CONNECT</span>
+                </StyledButtonConnect>
+            )}
+            {isConnected && (
+                <StyledButtonConnected onClick={() => disconnect()}>
+                    <StyledLogo src={userIcon} />
+                    <span className="address">{shortenAddress(address)}</span>
+                </StyledButtonConnected>
+            )}
+        </>
+    );
 };
 
 export default ConnectWallet;
 
 const StyledLogo = styled.img`
-  height: 22px;
-  margin-right: 2px;
+    height: 22px;
+    margin-right: 2px;
 `;
 
 const StyledButtonConnect = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0;
-  margin: 0;
-  color: #fff;
-  svg {
-    width: 18px;
-    margin-right: 6px;
-    path {
-      fill: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
+    margin: 0;
+    color: #fff;
+    svg {
+        width: 18px;
+        margin-right: 6px;
+        path {
+            fill: #fff;
+        }
     }
-  }
-  ${screenUp('lg')`
+    ${screenUp('lg')`
     .mobile {
       display: none ;
     }
@@ -77,16 +77,16 @@ const StyledButtonConnect = styled.button`
   `}
 `;
 const StyledButtonConnected = styled(StyledButtonConnect)`
-  .address {
-    font-weight: normal;
-  }
-  .web {
-    display: none;
-  }
-  :hover {
-    color: #6763e3;
-  }
-  border: 1px solid #363636;
-  padding: 3px 7px 3px 4px;
-  border-radius: 8px;
+    .address {
+        font-weight: normal;
+    }
+    .web {
+        display: none;
+    }
+    :hover {
+        color: #6763e3;
+    }
+    border: 1px solid #363636;
+    padding: 3px 7px 3px 4px;
+    border-radius: 8px;
 `;
