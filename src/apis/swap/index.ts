95a12c16b6f17b5ba0ae08c1906c8f4fadc50d31
Nguyễn Doãn Đạt <datndw@gmail.com>
2023-09-30 00:25:00 +0700

    datnd add tanstack

--- src/apis/swap/index.ts
+++ src/apis/swap/index.ts
@@ -1,5 +1,5 @@
 import { apiDeleteCall, apiGetCall, apiPostCall, apiPutCall } from ".."
-export const getSwapsByCondition = async (wallet: string, page: string) => {
+export const getSwapsByCondition = async (wallet: `0x${string}` | undefined, page: string) => {
     const response = await apiGetCall(`/api/Swap/${wallet}/${page}`)
     return response.data
 }
