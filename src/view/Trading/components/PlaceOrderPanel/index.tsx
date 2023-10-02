114c349d62044579ed911d4b2797793ae17588fb
HOANG\kenzj <hoangtth@f88.vn>
2023-10-02 20:19:21 +0700

    no message

--- src/view/Trading/components/PlaceOrderPanel/index.tsx
+++ src/view/Trading/components/PlaceOrderPanel/index.tsx
@@ -1,12 +1,92 @@
-import { memo } from "react";
+import './placeOrderPanel.scss'
+import { memo,useState } from "react";
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
 
 const PlaceOrderPanel: React.FC = () => {
-    return (
-      <>
-        PlaceOrderPanel
-      </>
-    );
+
+  const theme = useTheme();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
   };
-  
-  export default memo(PlaceOrderPanel);
-  
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <>
+      <Box sx={{ bgcolor: 'background.paper', width: 500 }}>
+        <AppBar position="static">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="secondary"
+            textColor="inherit"
+            aria-label="tabs example"
+          >
+            <Tab label="LONG" {...a11yProps(0)} />
+            <Tab label="SHORT" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={value}
+          onChangeIndex={handleChangeIndex}
+          style={{background:'#29292c'}}
+        >
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            Item One
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            Item Two
+          </TabPanel>
+        </SwipeableViews>
+      </Box>
+    </>
+  );
+};
+
+export default memo(PlaceOrderPanel);
